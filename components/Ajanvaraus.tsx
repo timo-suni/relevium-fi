ba4f62411ab733d8c5d31e2f8c64c33b2e2a9752
ctisu002xamk <ctisu002@edu.xamk.fi>
2023-06-06 16:14:07 +0300

    fix

--- components/Ajanvaraus.tsx
+++ components/Ajanvaraus.tsx
@@ -4,7 +4,7 @@ import Alert from './Alert'
 function Ajanvaraus() {
   return (
     <div>
-        <label htmlFor="ajanvaraus_modal" className="btn btn-sm bg-emerald-400 hover:bg-emerald-600 text-neutral-900 hover:text-neutral-200 font-semibold">Varaa Aika</label>
+        <label htmlFor="ajanvaraus_modal" className="btn btn-sm bg-emerald-400 hover:bg-emerald-600 text-neutral-800 hover:text-neutral-200 font-semibold">Varaa Aika</label>
         <input type="checkbox" id="ajanvaraus_modal" className="modal-toggle" />
         <div className="modal">
           <div className="modal-box bg-neutral-800 max-w-4xl">
